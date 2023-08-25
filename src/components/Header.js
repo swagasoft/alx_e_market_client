7810d1968f07b592c6169b66b68ee3025ec27f92
emarex1 <emmanuelawoh0@gmail.com>
2023-08-25 23:27:55 +0100

    create header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom';
-import { BsSearch } from "react-icons/bs";
+import { BsSearch, BsArrowRepeat } from "react-icons/bs";
 
 const Header = () => {
   return (
@@ -22,7 +22,7 @@ const Header = () => {
           <div className='row align-items-center'>
             <div className='col-2'>
               <h2>
-                <a className='text-white'>E-Market</a>
+                <Link className='text-white'>E-Market</Link>
               </h2>
             </div>
             <div className='col-5'><div className="input-group">
@@ -33,18 +33,62 @@ const Header = () => {
             <div className='col-5'>
               <div className='header-upper-links' d-flex align-items-center justify-conent-between>
                 <div>
-                  <link></link>
+                  <Link className='d-flex'>
+                    <img BsArrowRepeat />
+                    <p></p>
+                  </Link>
                 </div>
                 <div>
-                  <link></link>
+                  <Link>
+                    <img src='' alt=''></img>
+                  </Link>
                 </div>
                 <div>
-                  <link></link>
+                  <Link>
+                    <img src='' alt=''></img>
+                  </Link>
                 </div>
                 <div>
-                  <link></link>
+                  <Link>
+                    <img src='' alt=''></img>
+
+                  </Link>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+      </header >
+      <header className='header-bottom py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <div className='menu-bottom d-flex align-items-center gap-5'>
+                <div>
+                  <div className="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 
+                      d-flex align-items-center" type="button" id="dropdownMenuButton1"
+                      data-bs-toggle="dropdown" aria-expanded="false">
+                      <img src='images/menu.png' />
+                      <span className='me-3 inline-block'>Shop Categories</span>
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                      <li><Link className="dropdown-item text-white" to="">Another action</Link></li>
+                      <li><Link className="dropdown-item text-white" to="">Something else here</Link></li>
+                      <li><Link className="dropdown-item text-white" to="">Action</Link></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='menu-links'>
+                  <div className='d-flex align-items-center gap-5'>
+                    <NavLink to='/Home'> Home </NavLink>
+                    <NavLink to='/Our Store'> Our Store </NavLink>
+                    <NavLink to='/Blogs'> Blogs </NavLink>
+                    <NavLink to='/Contact'> Contact </NavLink>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>

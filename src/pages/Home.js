0911fab18668ce8870ca2afc425dafc4128b7026
emarex1 <emmanuelawoh0@gmail.com>
2023-08-31 18:52:56 +0100

    deleted image files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <section className='home-wrapper-1 py-5'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-6'>
+              <div className='main-banner-content py-3'>
+                <img src='images/image-2.jpg' style={{ width: "250px" }} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Home

984b9c8bbe8aa604afb12137cbe4028686f244ea
emarex1 <emmanuelawoh0@gmail.com>
2023-09-02 21:17:47 +0100

    home file

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { PRODUCTS } from '../products';
+
+import { Product } from "./product"
+
+const Home = () => {
+    return (
+        <>
+            <section className='home-wrapper-1 py-5'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-12'>
+                            <div className='shop'>
+                                <div className='shopTitle'>
+                                    <h> Emarex Shop</h>
+                                </div>
+                                <div className='products'> {PRODUCTS.map((product) => (
+                                    <Product data={product} />
+                                ))} </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Home

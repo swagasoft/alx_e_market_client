0bc36b18dcaf3190e3707b85297e81f577c22af2
emarex1 <emmanuelawoh0@gmail.com>
2023-09-12 19:11:24 +0100

    added functionality to add button

--- src/pages/product.js
+++ src/pages/product.js
@@ -3,7 +3,9 @@ import { ShopContext } from '../context/ShopContext';
 
 export const Product = (props) => {
     const { id, productName, price, productImage } = props.data;
-    const { addToCart } = useContext(ShopContext);
+    const { addToCart, cartItems } = useContext(ShopContext);
+
+    const cartItemsAmount = cartItems[id];
     return (
         <>
             <section className='home-wrapper'>
@@ -16,7 +18,7 @@ export const Product = (props) => {
                             <b>{productName}</b>
                         </p>
                         <p>₦{price}</p>
-                        <button className='toCart'>add to cart</button>
+                        <button className='toCart' onClick={() => addToCart(id)}>add to cart{cartItemsAmount > 0 && <>({cartItemsAmount})</>}</button>
                     </div>
                 </div>
             </section >
